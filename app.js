0d1fee6c3c8a131c24d8162e9bcd5536793af491
J-724 <jjpernian@gmail.com>
2023-01-30 22:09:12 -0400

    Fake data base created

--- app.js
+++ app.js
@@ -4,6 +4,43 @@ import express from 'express';
 
 const app = express();
 
+// <<<<<< Fake Database
+let users = {
+  1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+  3: {
+    id: '3',
+    username: 'Ryuk'
+  }
+}
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+  3: {
+    id: '3',
+    text: 'Quiero pollito !!!',
+    userId: '3',
+  }
+}
+//  <<<<<<
+
+
+
 app.use(cors());
 
 app.get('/', (req, res) => {
